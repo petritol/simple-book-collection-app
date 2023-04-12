4f92e04556f304ab5cef5f3617e4537ad1228aad
Petri Tolonen <petri.tolonen@buutti.com>
2023-04-12 18:57:34 +0300

    Condense form inputs into one state object

--- client/src/components/BookForm.jsx
+++ client/src/components/BookForm.jsx
@@ -2,18 +2,31 @@ import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 function BookForm(props) {
-    const [bookName, setBookName] = useState('');
-    const [authorName, setAuthorName] = useState('');
+    const [formData, setFormData] = useState({
+        bookName: '',
+        authorName: ''
+    });
 
     function handleSubmit(e) {
         e.preventDefault();
-        if (bookName.length === 0) {
+        if (formData.bookName.length === 0) {
             alert("Book name required!");
             return;
         }
-        props.addBook(bookName, authorName);
-        setBookName('');
-        setAuthorName('');
+        props.addBook(formData.bookName, formData.authorName);
+        setFormData({
+            bookName: '',
+            authorName: ''
+        });
+    }
+
+    function handleChange(e) {
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [e.target.name]: e.target.value
+            }
+        })
     }
 
     return(
@@ -23,16 +36,16 @@ function BookForm(props) {
                 <input
                     type="text"
                     name="bookName"
-                    value={bookName} 
-                    onChange={e => setBookName(e.target.value)}/>
+                    value={formData.bookName} 
+                    onChange={handleChange}/>
             </label>
             <label>
                 Author:
                 <input
                     type="text"
                     name="authorName"
-                    value={authorName}
-                    onChange={e => setAuthorName(e.target.value)} />
+                    value={formData.authorName}
+                    onChange={handleChange} />
             </label>
             <input type="submit" value="Submit" /> 
         </form>
